78bad055347d35bc11592ab822527cacf1def98a
Benjamin Dow <ben@bonta-kun.net>
2011-07-04 19:49:37 -0500

    Better organized tests and added a new to test say functionality, removed logging in irc library that was not needed anymore.

--- test/test.js
+++ test/test.js
@@ -5,16 +5,29 @@ require.paths.unshift(__dirname + '/../lib');
 
 var irc = require('irc');
 var assert = require('assert');
+var util = require('util');
 
-var defaultServer = 'irc.freenode.net';
-var defaultChannel = '#botwars';
-var defaultNick = 'nodeTestBot';
+////////
+// Constants/Defaults
+////////
+
+const defaultServer = 'irc.freenode.net';
+const defaultChannel = '#botwars';
+const defaultNick = 'nodeTestBot';
+
+////////
+// Helpers
+////////
 
 function getDefaultServerOptions() {
   return {
       port: 6665, secure: false, debug: true,
       retryCount: 0, channels: []};
 }
+
+////////
+// Validation code tests
+////////
 
 exports.testIrcEmptyServer = function() {
   assert.throws(function() {
@@ -37,9 +50,19 @@ exports.testIrcNickWithSpace = function() {
     /username contains a space\, this is invalid/);
 }
 
+////////
+// Insecure connection tests
+////////
+
 exports.testIrcFastConnectionDisconnect = function() {
   var bot = new irc.Client(defaultServer, defaultNick, getDefaultServerOptions());
   bot.disconnect();
+  
+  //these test internal implementation details I do not expect to change.
+  setTimeout(function() {
+  	assert.ok(!bot.conn.connected);
+  	assert.equal(bot.conn.readyState, "closed", "gave it 5 seconds to close the connection and tidy up, it appears to have taken longer, investigate");
+  }, 5000);
 }
 
 exports.testIrcChannelJoin = function() {
@@ -50,12 +73,16 @@ exports.testIrcChannelJoin = function() {
 
   var bot = new irc.Client(defaultServer, defaultNick, connectOptions);
   bot.addListener('join', function(channel, who) {
-    console.log('%s has joined %s', who, channel);
     channelJoined = true;
     bot.disconnect();
   });
+  
   setTimeout(function() {assert.ok(channelJoined, "gave it 15 seconds to connect and join the channel, looks like that failed for some reason, double check the logs.");}, 15000);
 }
+
+////////
+// Secure connection tests
+////////
 
 exports.testIrcFastConnectionDisconnectSecure = function() {
   var connectOptions = getDefaultServerOptions();
@@ -64,4 +91,83 @@ exports.testIrcFastConnectionDisconnectSecure = function() {
 
   var bot = new irc.Client(defaultServer, defaultNick, connectOptions);
   bot.disconnect();
+  
+  //these test internal implementation details I do not expect to change.
+  setTimeout(function() {
+  	assert.ok(!bot.conn.connected);
+  	assert.equal(bot.conn.readyState, "closed", "gave it 5 seconds to close the connection and tidy up, it appears to have taken longer, investigate");
+  }, 5000);
+}
+
+exports.testIrcChannelJoinSecure = function() {
+  var connectOptions = getDefaultServerOptions();
+  connectOptions.channels.push(defaultChannel);
+  
+  var channelJoined = false;
+
+  var bot = new irc.Client(defaultServer, defaultNick, connectOptions);
+  bot.addListener('join', function(channel, who) {
+    channelJoined = true;
+    bot.disconnect();
+  });
+  
+  setTimeout(function() {assert.ok(channelJoined, "gave it 15 seconds to connect and join the channel, looks like that failed for some reason, double check the logs.");}, 15000);
+}
+
+////////
+// Functional tests
+////////
+
+
+/* This test is ambitious enough that it has earned a comment, it tests a secure 
+ * and insecure bot talking to each other. Joining a channel and listening 
+ * to what each other say.
+ */
+
+exports.testIrcTalkingInChannel = function() {
+	var insecureBotIn = false;
+	var secureBotIn = false;
+	
+	var haveConversation = function () {
+		assert.ok(insecureBot.conn.connected);
+		assert.ok(secureBot.conn.connected);
+		
+		//setup listeners to check incoming messages
+		insecureBot.addListener('message', function (from, to, message) {
+			if (from.match(/nodeTestBot[0-9]*/))
+    		assert.equal(message, "hello insecureBot");
+    		insecureBot.disconnect();
+		});
+		secureBot.addListener('message', function (from, to, message) {
+			if (from.match(/nodeTestBot[0-9]*/))
+    		assert.equal(message, "hello secureBot")
+    		secureBot.disconnect();
+		});
+		
+		insecureBot.say(defaultChannel, "hello secureBot");
+		secureBot.say(defaultChannel, "hello insecureBot");
+	}
+
+  var connectOptions = getDefaultServerOptions();
+  connectOptions.channels.push(defaultChannel);
+
+  var insecureBot = new irc.Client(defaultServer, defaultNick, connectOptions);
+  insecureBot.addListener('join', function(channel, who) {
+    insecureBotIn = true;
+    if (insecureBotIn && secureBotIn)
+    	haveConversation();
+  });
+
+	connectOptions = getDefaultServerOptions();
+  connectOptions.channels.push(defaultChannel);
+	connectOptions.secure = true;
+  connectOptions.port = 6697;
+
+  var secureBot = new irc.Client(defaultServer, defaultNick, connectOptions);
+  secureBot.addListener('join', function(channel, who) {
+    secureBotIn = true;
+    if (insecureBotIn && secureBotIn)
+    	haveConversation();
+  });
+  
 }
